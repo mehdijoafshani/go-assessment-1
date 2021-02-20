b99b27ebdff69cf36ab73b5ea47cbb0612114897
mehdijoafshani <mahdi.joafshani@gmail.com>
2021-02-20 15:17:49 +0330

    initialize rest api

--- api/rest.go
+++ api/rest.go
@@ -2,19 +2,38 @@ package api
 
 import (
 	"github.com/gin-gonic/gin"
-	"net/http"
 )
+
+func create(c *gin.Context) {
+	//TODO impl
+}
+
+func getBalance(c *gin.Context) {
+	//TODO impl
+}
+
+func getAllBalances(c *gin.Context) {
+	//TODO impl
+}
+
+func addBalance(c *gin.Context) {
+	//TODO impl
+}
+
+func addToAllBalances(c *gin.Context) {
+	//TODO impl
+}
 
 func StartRestServer() error {
 	gin.SetMode(gin.ReleaseMode)
 	// TODO fix gin logger
 
 	r := gin.New()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "pong",
-		})
-	})
+	r.POST("/create", create)
+	r.GET("/getBalance", getBalance)
+	r.GET("/getAllBalances", getAllBalances)
+	r.PUT("/addBalance", addBalance)
+	r.PUT("/addToAllBalances", addToAllBalances)
 
 	err := r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 	if err != nil {
