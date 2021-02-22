42a134df88f3d8c3e9ba540b620f88c96c4ce89d
mehdijoafshani <mahdi.joafshani@gmail.com>
2021-02-22 23:45:39 +0330

    add comment

--- balance/batch.go
+++ balance/batch.go
@@ -1,5 +1,11 @@
 package balance
 
+// #SOLID: O
+// Instead of relying on a concrete struct and implement createBalances, ... methods, an interface has been defined
+// This will give us the power of changing the functionality with bringing new solutions (extending the usage of this interface instead of changing single implementation)
+// This indicates that Open/Closed principle has been applied here
+// We have to pay attention that critical changes should be applied and extending is not for all cases
+
 // the operation which process a number of procedure
 // (the procedure could be performed either concurrently or serially)
 type batchOperationManager interface {
