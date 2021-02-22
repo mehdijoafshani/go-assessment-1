c35aa7ff37ad576eb006ccfd95a39b5b50836a5b
mehdijoafshani <mahdi.joafshani@gmail.com>
2021-02-22 23:18:05 +0330

    add handle interrupt code

--- main/main.go
+++ main/main.go
@@ -12,10 +12,16 @@ func main() {
 	config.SetupViper(".")
 	logger.SetupZap()
 
+	//c := make(chan os.Signal, 1)
+	//signal.Notify(c, os.Interrupt)
+	//go func(){
+	//	for sig := range c {
+	//		// TODO: close possible opened connections
+	//	}
+	//}()
+
 	err := api.StartRestServer()
 	if err != nil {
 		logger.Zap().Fatal("failed to start REST service", zap.Error(err))
 	}
-
-	// TODO: close possible opened connections
 }
