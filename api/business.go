bd6fa4c99a320f12bc469439598099ea29fa3e40
mehdijoafshani <mahdi.joafshani@gmail.com>
2021-02-22 23:30:28 +0330

    add solid principle comments

--- api/business.go
+++ api/business.go
@@ -1,5 +1,15 @@
 package api
 
+// #SOLID: S
+// The only possible reason to change this file is to change interfaces related to the business layer
+// This indicates that the single responsibility principle has been applied
+
+// #SOLID: D
+// The API code depends on this interface, the interface would be implemented by lower level layers
+// This means that API code (higher-level code) is not depended on the lower-level layer, instead the lower-level needs to satisfy higher-level api
+// The terminology used in the interface is based on the current layer's terminology, so the current layer would not be affected by the changes in lower-layer code
+// And this is an example of applying Dependency Inversion principle
+// This pattern has been repeated throughout the project
 type balanceManager interface {
 	Create(accountsNum int) error
 	GetAll() (int64, error)
