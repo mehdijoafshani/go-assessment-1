84df93e3b97488d40dbbf6ecaec26efdd89f0c44
mehdijoafshani <mahdi.joafshani@gmail.com>
2021-02-23 04:19:30 +0330

    add doc

--- api/doc.go
+++ api/doc.go
@@ -1,2 +1,4 @@
-//TODO package doc
+// API package is the highest possible level in the project
+// The package only provides REST api at the moment. However it is open to more technologies such as gPRS.
+// TODO enhance documentation
 package api
