eb1bfab95a9fefc9bac8be1f53abfebde792438d
mehdijoafshani <mahdi.joafshani@gmail.com>
2021-02-23 12:47:12 +0330

    change api to follow REST best practices

--- api/rest.go
+++ api/rest.go
@@ -17,6 +17,11 @@ import (
 // #Desc
 // Maybe defining a separate struct, including the following manager var inside it and implement an interface would sound a good idea
 // However, each API (REST, gRPC, ...) would need a separate set of arguments in their methods, I decided to keep it as it is.
+
+const (
+	noId = -1
+)
+
 var manager balanceManager
 
 func accountsPost(c *gin.Context) {
@@ -44,33 +49,36 @@ func accountsPost(c *gin.Context) {
 	c.String(http.StatusOK, "%d accounts are created", number)
 }
 
-func balanceGet(c *gin.Context) {
-	idStr := c.Query("id")
-
-	id, err := strconv.Atoi(idStr)
-	if err != nil {
-		logger.Zap().Debug("non-numeric parameter for an account's id to get balance",
-			zap.String("id", idStr),
-			zap.Error(err))
-
-		c.String(http.StatusUnprocessableEntity, "id ought to be numeric")
-		return
-	}
-
-	balance, err := manager.Get(id)
-	if err != nil {
-		logger.Zap().Error("failed to get balance", zap.Error(err))
-		// TODO: return more explicit error code
-		c.String(http.StatusInternalServerError, "failed to get balance")
-		return
-	}
-
-	c.String(http.StatusOK, "%d", balance)
-}
-
-func balancesGet(c *gin.Context) {
+// #Desc
+// To follow REST best practices the endpoints represent resources, that's why one func (accountsGet for instance) is declared to
+// get one account's balance, all balances' sum or all balances in list
+// I also could have separate them into sub functions to better segregation
+func accountsGet(c *gin.Context) {
 	res := c.Query("result")
 
+	idStr := c.Query("id")
+
+	var id int
+	var err error
+	id, err = strconv.Atoi(idStr)
+	if err != nil {
+		id = noId
+	}
+
+	// if there is an id in the request (getting only one balance)
+	if id != noId {
+		balance, err := manager.Get(id)
+		if err != nil {
+			logger.Zap().Error("failed to get balance", zap.Error(err))
+			// TODO: return more explicit error code
+			c.String(http.StatusInternalServerError, "failed to get balance")
+			return
+		}
+		c.String(http.StatusOK, "%d", balance)
+		return
+	}
+
+	// getting all balances
 	switch res {
 	// to return sum of all balances
 	case "aggregate":
@@ -92,48 +100,9 @@ func balancesGet(c *gin.Context) {
 	}
 }
 
-func balancePost(c *gin.Context) {
+func accountsPut(c *gin.Context) {
+	balanceS := c.Query("increase")
 	idS := c.Query("id")
-	balanceS := c.Query("increase")
-
-	id, err := strconv.Atoi(idS)
-	if err != nil {
-		logger.Zap().Debug("non-numeric parameter for an account's id to add balance",
-			zap.String("id", idS),
-			zap.Error(err))
-
-		c.String(http.StatusUnprocessableEntity, "id ought to be numeric")
-		return
-	}
-
-	balance, err := strconv.Atoi(balanceS)
-	if err != nil {
-		logger.Zap().Debug("non-numeric parameter for the amount to add balance",
-			zap.String("balance", balanceS),
-			zap.Error(err))
-
-		c.String(http.StatusUnprocessableEntity, "extra balance ought to be numeric")
-		return
-	}
-
-	if balance < 0 {
-		c.String(http.StatusUnprocessableEntity, "extra balance ought to be positive")
-		return
-	}
-
-	err = manager.Add(balance, id)
-	if err != nil {
-		logger.Zap().Error("failed to add balance", zap.Error(err))
-		// TODO: return more explicit error code
-		c.String(http.StatusInternalServerError, "failed to add balance")
-		return
-	}
-
-	c.String(http.StatusOK, "the extra balance %d  is applied to id %d", balance, id)
-}
-
-func balancesPost(c *gin.Context) {
-	balanceS := c.Query("increase")
 
 	balance, err := strconv.Atoi(balanceS)
 	if err != nil {
@@ -144,17 +113,32 @@ func balancesPost(c *gin.Context) {
 		return
 	}
 
-	if balance < 0 {
-		c.String(http.StatusUnprocessableEntity, "extra balance ought to be positive")
-		return
+	var id int
+	id, err = strconv.Atoi(idS)
+	if err != nil {
+		id = noId
 	}
 
-	err = manager.AddToAll(balance)
-	if err != nil {
-		logger.Zap().Error("failed to add balance", zap.Error(err))
-		// TODO: return more explicit error code
-		c.String(http.StatusInternalServerError, "failed to add balance")
-		return
+	switch id {
+	case noId:
+		//add to all balances
+		err = manager.AddToAll(balance)
+		if err != nil {
+			logger.Zap().Error("failed to add balance", zap.Error(err))
+			// TODO: return more explicit error code
+			c.String(http.StatusInternalServerError, "failed to add balance")
+			return
+		}
+	default:
+		// apply on one specific balance
+		err = manager.Add(balance, id)
+		if err != nil {
+			logger.Zap().Error("failed to add balance", zap.Error(err))
+			// TODO: return more explicit error code
+			c.String(http.StatusInternalServerError, "failed to add balance")
+			return
+		}
+
 	}
 
 	c.String(http.StatusOK, "the extra balance %d is applied to all accounts", balance)
@@ -168,10 +152,8 @@ func StartRestServer() error {
 
 	r := gin.New()
 	r.POST("/accounts", accountsPost)
-	r.GET("/balance", balanceGet)
-	r.GET("/balances", balancesGet)
-	r.PUT("/balance", balancePost)
-	r.PUT("/balances", balancesPost)
+	r.GET("/accounts", accountsGet)
+	r.PUT("/accounts", accountsPut)
 
 	err := r.Run(":" + config.Data.RestPort)
 	if err != nil {
