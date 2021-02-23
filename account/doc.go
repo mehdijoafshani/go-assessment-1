9bb457d5ccbb97c418d8a398537c54cbd6697194
mehdijoafshani <mahdi.joafshani@gmail.com>
2021-02-23 04:17:03 +0330

    add doc

--- account/doc.go
+++ account/doc.go
@@ -1,2 +1,6 @@
-//TODO addBalance package doc
+// Business layer
+// Account package would manage all the business logics behind the API
+// This module would be called by the higher level (api package), and it satisfies the api package requirements
+// The package declares and only relies on its requirements (interfaces) that lower-level modules should implement
+// TODO enhance documentation
 package account
